4ed5a731d7f62f7b366eef982b0be8dee7ab2007
Krindendo work <lollipopfunkyx9@gmail.com>
2024-03-25 00:05:32 +0100

    chore: remove unnecessary files

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-between p-24">
-      <h1>Next + MDX</h1>
-    </div>
-  );
-}
