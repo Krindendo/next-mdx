da6dcb57788731bc2b3dcd331560b41b9a6eed31
Krindendo work <lollipopfunkyx9@gmail.com>
2024-03-26 00:07:42 +0100

    feat: add basic layouts and providers

---
+++ layouts/Base.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import type { FC, PropsWithChildren } from 'react';
+
+
+import styles from './layouts.module.css';
+
+const BaseLayout: FC<PropsWithChildren> = ({ children }) => (
+
+    <div className={styles.baseLayout}>{children}</div>
+
+);
+
+export default BaseLayout;
